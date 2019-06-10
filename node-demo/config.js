aa4154f7ae14adecb10e8bedcbb2485d1c38b0e6
Naixes <615411375@qq.com>
2019-06-11 06:49:49 +0800

    node-demo init

--- node-demo/config.js
+++ node-demo/config.js
@@ -2,7 +2,9 @@ module.exports = {
     DB_HOST: 'localhost',
     DB_USER: 'admin',
     DB_PASS: 'root',
-    DN_NAME: 'node'
+    DN_NAME: 'node',
 
+		HTTP_ROOT: 'http://localhost:8080/',
+		// HTTP_ROOT: 'https://www.aaa.com/'
 
 }
