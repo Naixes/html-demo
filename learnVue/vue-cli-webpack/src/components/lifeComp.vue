08649815b685e6e9141e01648be119ffb87b3859
Naixes <615411375@qq.com>
2019-06-25 15:45:57 +0800

    路由守卫与生命周期

---
+++ learnVue/vue-cli-webpack/src/components/lifeComp.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="compLifecircle">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  // 组件内路由守卫
+  beforeRouteEnter (to, from, next) {
+    console.log('comp beforeRouteEnter')
+    next(vm => {
+      console.log(vm)
+    })
+  },
+  beforeRouteUpdate (to, from, next) {
+    console.log('comp beforeRouteUpdate')
+  },
+  beforeCreate () {
+    console.log('comp beforeCreate')
+  },
+  data () {
+    console.log('comp data')
+    return {
+      msg: ' : this is comp msg'
+    }
+  },
+  created () {
+    console.log(this.msg)
+    console.log('comp created')
+  },
+  beforeMount () {
+    console.log('comp beforeMount')
+  },
+  // render() {
+  //     console.log("render")
+  // },
+  mounted () {
+    console.log('comp mounted')
+    this.msg = 'comp change'
+  },
+  beforeUpdate () {
+    console.log('comp beforeUpdate')
+  },
+  updated () {
+    console.log('comp updated')
+    // this.$destroy()
+  },
+  beforeDestroy () {
+    console.log('comp beforeDestroy')
+  },
+  destroyed () {
+    console.log('comp destroyed')
+  }
+}
+</script>
+
+<style>
+
+</style>
