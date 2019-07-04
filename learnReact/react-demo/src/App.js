c348b4248942b291f73d534ac178de39d9559f38
Naixes <615411375@qq.com>
2019-07-04 14:56:01 +0800

    create-react-app init

---
+++ learnReact/react-demo/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Comp from './Comp'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        aaa
+        <Comp/>
+      </div>
+    )
+  }
+}
+
+export default App;
