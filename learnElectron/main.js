9096b49fd4207af26cbee0b59d691306d9a10960
Naixes <615411375@qq.com>
2019-08-22 15:18:46 +0800

    加入贪吃蛇代码

--- learnElectron/main.js
+++ learnElectron/main.js
@@ -3,13 +3,12 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {   
   // 创建浏览器窗口
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 830,
+    height: 680,
     webPreferences: {
       nodeIntegration: true
     }
   })
-
   // 加载index.html文件
   win.loadFile('index.html')
 }
