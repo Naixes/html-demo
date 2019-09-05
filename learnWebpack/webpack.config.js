b859f284a2e16eea40206380024d50b37a7d802b
Naixes <615411375@qq.com>
2019-09-06 07:01:57 +0800

    css loader

--- learnWebpack/webpack.config.js
+++ learnWebpack/webpack.config.js
@@ -30,6 +30,34 @@ module.exports = {
 			hash: true
 		})
 	],
+	module: { // 用来配置第三方loader模块的
+		rules: [
+			// 文件的匹配规则，从右到左依次处理
+			// loader功能单一，可以叠加使用
+			// css-loader： 加载css文件，成为js的一部分（读取成字符串），可以解析@import这种语法，解析路径
+			// style-loader： 使css有作用，让样式字符串变成style标签输出到页面
+			// 一个loader写成字符串，顺序：从右向左，从下到上
+			// { test: /\.css$/, use: ['style-loader', 'css-loader'] } //处理css文件的规则
+			// 对象形式：可以传多个参数
+			{
+				test: /\.css$/, use: [{
+					loader: 'style-loader',
+					options: {
+						// 让样式字符串变成style标签输出到页面，header标签的顶部，不会覆盖自定义样式
+						insertAt: 'top'
+					}
+				}, 'css-loader']
+			},
+			{
+				test: /\.less$/, use: [{
+					loader: 'style-loader',
+					options: {
+						insertAt: 'top'
+					}
+				}, 'css-loader', 'less-loader']
+			}
+		]
+	},
 	// production(默认) development
 	mode: 'development',
 	// 入口：默认就是这个
