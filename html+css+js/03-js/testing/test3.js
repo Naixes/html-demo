f96a045ca60f5fb5eaf465b76ddfe7803de695d7
Naixes <615411375@qq.com>
2019-11-19 08:10:58 +0800

    GC

--- html+css+js/03-js/testing/test3.js
+++ html+css+js/03-js/testing/test3.js
@@ -23,8 +23,33 @@ s3 = null
 function test() {
 	var a = "yideng";
 	return function () {
-		// eval：可以欺骗此法作用域，不会对词法作用域的任何变量解除绑定，即就算没有引用，也不会被回收，因为里面是字符串，浏览器不知道里面是否有引用
+		// 1. eval：可以欺骗此法作用域，不会对词法作用域的任何变量解除绑定，即就算没有引用，也不会被回收，因为里面是字符串，浏览器不知道里面是否有引用
 		eval("");
 	}
 }
 test()();
+
+var obj = {}
+// 2. with会放弃所有变量回收
+with (obj) {
+	a = 1
+}
+console.log(obj.a) // undefined
+console.log(a) // 1
+
+function init() {
+	var a = 30
+	// 3. new Function 绑定全局作用域
+	var s = new Function('console.log(a)')
+	s()
+}
+init() // 20
+
+function init() {
+	var a = 30
+	var s = new Function(console.log(a))
+	s()
+}
+init() // 30
+
+// 4. try-catch 的 catch 也欺骗了此法作用域（e）
