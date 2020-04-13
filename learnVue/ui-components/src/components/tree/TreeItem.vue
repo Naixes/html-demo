1c04dcf29248527e447e95592a9e409e14c203f1
Naixes <615411375@qq.com>
2020-04-13 14:30:02 +0800

    tree

--- learnVue/ui-components/src/components/tree/TreeItem.vue
+++ learnVue/ui-components/src/components/tree/TreeItem.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-      <li></li>
+      <li :fold="fold">
+        {{model.value}}
+        <span @click="toggle" v-if="model.children">[{{fold ? '-' : '+'}}]</span>
+        <ul v-if="model.children && fold">
+          <item v-for="mod in model.children" :model="mod" :key="mod.value"></item>
+        </ul>
+      </li>
     </div>
 </template>
 
 <script>
 
 export default {
+  name: 'item',
   props: {
     model: {
       type: Object,
@@ -17,9 +24,13 @@ export default {
   },
   data() {
     return {
+      fold: false,
     };
   },
   methods: {
+    toggle() {
+      this.fold = !this.fold;
+    },
   },
 };
 </script>
