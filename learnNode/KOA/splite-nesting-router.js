b0f0c27c4c53699f352fbcdf62f72795d61f8520
Naixes <615411375@qq.com>
2021-03-09 18:04:01 +0800

    koa

--- learnNode/KOA/splite-nesting-router.js
+++ learnNode/KOA/splite-nesting-router.js
@@ -6,6 +6,9 @@ server.listen(8080)
 
 let router = new Router()
 
+// 路径前缀设置
+router.prefix = '/api'
+
 router.get('/a', ctx => {
     ctx.body = 'aa'
     ctx.body += 'bb'
@@ -13,3 +16,5 @@ router.get('/a', ctx => {
 router.use('/user', require('./routers/user'))
 
 server.use(router.routes())
+    // 拦截器，拦截未匹配到的请求返回4xx或者5xx的错误
+    .use(router.allowedMethods())
