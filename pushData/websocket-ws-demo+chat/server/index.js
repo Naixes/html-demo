4eb08bf242c2805c983a74ef469cf17ef976493c
Naixes <615411375@qq.com>
2021-05-02 11:27:22 +0800

    多聊天室

--- pushData/websocket-ws-demo+chat/server/index.js
+++ pushData/websocket-ws-demo+chat/server/index.js
@@ -3,36 +3,40 @@ const Websocket = require('ws')
 const wss = new Websocket.Server({port: 3000})
 
 let num = 0
+let group = {}
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function (msg) {
         const msgObj = JSON.parse(msg)
         if(msgObj.type === 'enter') {
-            num++
-            console.log('num', num);
+            if(typeof group[msgObj.roomid] === 'undefined') {
+                group[msgObj.roomid] = 1
+            }else {
+                group[msgObj.roomid]++
+            }
             ws.name = msgObj.name
+            ws.roomid = msgObj.roomid
         }
+        // 广播消息
         wss.clients.forEach(client => {
             // 连接人数
             // wss.clients.size
-            // 广播消息
             // 客户端需要过滤掉自己
-            if(client.readyState == Websocket.OPEN) {
-                msgObj.num = num
-                console.log('msgObj', msgObj);
+            if(client.readyState == Websocket.OPEN && client.roomid === ws.roomid) {
+                msgObj.num = group[msgObj.roomid]
                 client.send(JSON.stringify(msgObj))
             }
         })
     })
-    ws.on('close', function (msg) {
+    ws.on('close', function () {
         if(ws.name) {
-            num--
+            group[ws.roomid]--
         }
         wss.clients.forEach(client => {
-            if(client.readyState == Websocket.OPEN) {
+            if(client.readyState == Websocket.OPEN && client.roomid === ws.roomid) {
                 const msgObj = {
                     name: ws.name,
-                    num,
+                    num: group[ws.roomid],
                     type: 'out'
                 }
                 client.send(JSON.stringify(msgObj))
