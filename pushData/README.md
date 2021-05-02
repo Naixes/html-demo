b2d339fca0d41be996dc8447803bd058121a5bf6
Naixes <615411375@qq.com>
2021-05-02 10:51:28 +0800

    统计在线人数

--- pushData/README.md
+++ pushData/README.md
@@ -2,8 +2,8 @@
 
 ### Comet
 
-###websocket
+### websocket
 
-执行express命令利用脚手架生成项目框架（也可能是别的命令，具体查看官网）
+#### socket.io
 
-安装socket.io
+#### ws
--- pushData/websocket-ws-demo+chat/server/index.js
+++ pushData/websocket-ws-demo+chat/server/index.js
@@ -2,13 +2,40 @@ const Websocket = require('ws')
 
 const wss = new Websocket.Server({port: 3000})
 
+let num = 0
+
 wss.on('connection', function connection(ws) {
     ws.on('message', function (msg) {
-        console.log('msg', msg);
+        const msgObj = JSON.parse(msg)
+        if(msgObj.type === 'enter') {
+            num++
+            console.log('num', num);
+            ws.name = msgObj.name
+        }
         wss.clients.forEach(client => {
-            // 过滤掉自己
-            if(ws !== client && client.readyState == Websocket.OPEN) {
-                client.send(msg)
+            // 连接人数
+            // wss.clients.size
+            // 广播消息
+            // 客户端需要过滤掉自己
+            if(client.readyState == Websocket.OPEN) {
+                msgObj.num = num
+                console.log('msgObj', msgObj);
+                client.send(JSON.stringify(msgObj))
+            }
+        })
+    })
+    ws.on('close', function (msg) {
+        if(ws.name) {
+            num--
+        }
+        wss.clients.forEach(client => {
+            if(client.readyState == Websocket.OPEN) {
+                const msgObj = {
+                    name: ws.name,
+                    num,
+                    type: 'out'
+                }
+                client.send(JSON.stringify(msgObj))
             }
         })
     })
