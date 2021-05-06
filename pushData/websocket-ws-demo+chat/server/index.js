32fa174fda2d50fd1299fa1b77f9bfce70776e6a
Naixes <615411375@qq.com>
2021-05-06 21:29:15 +0800

    鉴权&心跳检测

--- pushData/websocket-ws-demo+chat/server/index.js
+++ pushData/websocket-ws-demo+chat/server/index.js
@@ -1,13 +1,43 @@
-const Websocket = require('ws')
+const WebSocket = require('ws')
+const http = require('http');
+const jwt = require('jsonwebtoken')
 
-const wss = new Websocket.Server({port: 3000})
+const server = http.createServer();
+// const wss = new WebSocket.Server({port: 3000})
+const wss = new WebSocket.Server({ noServer: true });
 
 let num = 0
 let group = {}
+const heartbeatTime = 2000
 
 wss.on('connection', function connection(ws) {
+    // 初始化心跳检测
+    ws.isAlive = true
+
     ws.on('message', function (msg) {
         const msgObj = JSON.parse(msg)
+        // 判断客户端主动发送的鉴权消息
+        if(msgObj.type === 'auth') {
+            jwt.verify(msgObj.message, 'secret', (err, decode) => {
+                if(err) {
+                    ws.send({
+                        type: 'noauth',
+                        message: 'please auth again'
+                    })
+                    console.log('鉴权失败');
+                    return
+                }else {
+                    console.log(decode);
+                    ws.isAuth = true
+                    return
+                }
+            })
+            return
+        }
+        // 拦截未鉴权消息
+        if(!ws.isAuth) {
+            return
+        }
         if(msgObj.type === 'enter') {
             if(typeof group[msgObj.roomid] === 'undefined') {
                 group[msgObj.roomid] = 1
@@ -17,13 +47,18 @@ wss.on('connection', function connection(ws) {
             ws.name = msgObj.name
             ws.roomid = msgObj.roomid
         }
+        // 心跳检测
+        if(msgObj.type === 'heartbeat' && msgObj.message === 'pong') {
+            ws.isAlive = true
+            return
+        }
         // 广播消息
         wss.clients.forEach(client => {
             // 连接人数
             // wss.clients.size
             // 客户端需要过滤掉自己
-            if(client.readyState == Websocket.OPEN && client.roomid === ws.roomid) {
-                msgObj.num = group[msgObj.roomid]
+            if(client.readyState == WebSocket.OPEN && client.roomid === ws.roomid) {
+                msgObj.num = group[ws.roomid]
                 client.send(JSON.stringify(msgObj))
             }
         })
@@ -33,7 +68,7 @@ wss.on('connection', function connection(ws) {
             group[ws.roomid]--
         }
         wss.clients.forEach(client => {
-            if(client.readyState == Websocket.OPEN && client.roomid === ws.roomid) {
+            if(client.readyState == WebSocket.OPEN && client.roomid === ws.roomid) {
                 const msgObj = {
                     name: ws.name,
                     num: group[ws.roomid],
@@ -44,3 +79,36 @@ wss.on('connection', function connection(ws) {
         })
     })
 })
+
+server.on('upgrade', function upgrade(request, socket, head) {
+    // This function is not defined on purpose. Implement it with your own logic.
+    // authenticate(request, (err, client) => {
+    //     if (err || !client) {
+    //         socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+    //         socket.destroy();
+    //         return;
+    //     }
+
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request);
+        });
+    // });
+});
+
+server.listen(3000);
+
+// 心跳检测
+setInterval(() => {
+    wss.clients.forEach(ws => {
+        if(!ws.isAlive) {
+            group[ws.roomid]--
+            return ws.terminate()
+        }
+        ws.isAlive = false
+        ws.send(JSON.stringify({
+            type: 'heartbeat',
+            message: 'ping',
+            num: group[ws.roomid]
+        }))
+    })
+}, heartbeatTime)
