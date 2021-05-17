79b00eef86dee5b79e544238bd7f03f1863f52c8
Naixes <615411375@qq.com>
2021-05-17 10:10:37 +0800

    del

--- learnVue/vue-ssr-nuxt/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-// axios已内置
-// 拦截器
-export default function ({ $axios, redirect }) {
-  $axios.onError((error) => {
-    if (error.response.status === 500) {
-      redirect('/sorry')
-    }
-  })
-}
